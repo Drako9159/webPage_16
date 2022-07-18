f9f9cc5d2f6274eae716a9ce9ae4c46a4d7821a8
drako9159 <chochito9159@gmail.com>
2022-07-17 23:47:52 -0500

    add page

---
+++ src/views/table.vue
@@ -0,0 +1,64 @@
+<template>
+  <section id="bannerT">
+    <table>
+      <tr id="tableId">
+        <th>ID</th>
+        <th>Jugador</th>
+        <th>Nombre</th>
+        <th>Versión</th>
+        <th>Arma</th>
+        <th></th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+       
+      </tr>
+      <tr>
+        <td>New</td>
+        <td>Mexico</td>
+      </tr>
+      <td>
+        <td>lol</td>
+
+      </td>
+      <td>
+        <td><button>Eliminar</button><button>Modificar</button></td>
+        <td><button>Eliminar</button><button>Modificar</button></td>
+      </td>
+    </table>
+  </section>
+</template>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    color: black
+}
+#bannerT{
+    margin: 0 auto;
+    background-color: rgb(241, 241, 241);
+}
+#bannerT table tr th{
+    padding: 0px;
+    margin: 0px;
+    border-style: solid;
+    border-color: black;
+    border-width: 2px;
+}
+#bannerT table tr td{
+    margin: 0;
+    border-style: solid;
+    border-color: black;
+    border-width: 1px;
+}
+#bannerT table tr{
+    margin: 0px;
+    padding: 0px;
+}
+
+</style>
