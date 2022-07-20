f4d3ba07df061f14a7e5b5f9cbda6fddd5a2a306
drako9159 <chochito9159@gmail.com>
2022-07-20 14:45:12 -0500

    refact

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,19 @@ import HelloWorld from "./components/HelloWorld.vue";
     <header>
       <nav>
         <div class="mobile">
+          <div class="header">
+            <button id="bmenu">
+              <a href="#"><img src="../src/assets/icons8-menú-30.png"/></a>
+            </button>
+            <img class="logs" src="../src/assets/logs.png"/>
+            <img class="logs1" src="../src/assets/user.png"/>
+          </div>
+          <div class="links">
+            <a href="#">game pass</a>
+            <a href="#">game pass</a>
+            <a href="#">game pass</a>
+            <a href="#">game pass</a>
+          </div>
         </div>
         <ul>
           <li><a href="" class="link">Saludos</a></li>
@@ -35,7 +48,6 @@ import HelloWorld from "./components/HelloWorld.vue";
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -48,13 +60,18 @@ import HelloWorld from "./components/HelloWorld.vue";
 </template>
 <style>
 @import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+@font-face{
+  font-family: monste;
+  src: url('../src/assets/montserrat/Montserrat-Medium.otf');
+}
 body {
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: monste;
   font-size: 16px;
   padding: 0;
   margin: 0;
   line-height: 1.5;
 }
+
 /* navbar */
 nav {
   display: flex;
@@ -92,12 +109,63 @@ nav ul li .link::before{
 nav ul li .link:hover::before{
     transform: scaleX(1);
 }
+nav .mobile{
+  display: none;
+  width: 100%;
+}
+nav .mobile .header{
+  display: flex;
+  text-align: center;
+  justify-content: space-between;
+  flex-direction: row;
+  background-color: red;
+}
+nav .mobile .header .logs{
+  width: 120px;
 
-
+}
+nav .mobile .header .logs1{
+  padding: 10px 10px 0px 0px;
+  
+  width: 40px;
+  height: 40px;
+}
+nav .mobile .header #bmenu{
+  border: none;
+  cursor: pointer;
+  color: grey;
+}
+nav .mobile .links{
+  display: none;
+  flex-direction: column;
+  position: relative;
+  z-index: 200;
+  background-color: black;
+}
+nav .mobile .links{
+  font-size: 20px;
+  display: block;
+  padding: 10px 20px;
+  text-decoration: none;
+  color: white;
+}
+nav .mobile .links a:hover{
+  font-size: 20px;
+  display: block;
+  padding: 10px 20px;
+  text-decoration: none;
+  color: white;
+}
 @media screen and (max-width: 701px){
+  nav .mobile{
+    display: block;
+  }
   nav{
     width: 100%;
     padding: 0;
+  }
+  nav ul{
+    display: none;
   }
 }
 </style>
